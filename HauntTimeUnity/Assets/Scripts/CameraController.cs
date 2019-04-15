50b81a34193149b4dafc3b5485c6f199ede8c288
Drew Graham <Drew Graham>
2019-04-15 14:01:11 -0400

    created simple CameraController (press A/D to move to left/right rooms); moved Rush's scripts to their own folder

---
+++ HauntTimeUnity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform leftRoomViewpoint;
+    public Transform rightRoomViewpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            MoveCamera(leftRoomViewpoint);
+        }
+        else if(Input.GetKeyDown(KeyCode.D)) {
+            MoveCamera(rightRoomViewpoint);
+        }
+    }
+
+    void MoveCamera(Transform room)
+    {
+        Camera.main.transform.position = room.position;
+    }
+}
