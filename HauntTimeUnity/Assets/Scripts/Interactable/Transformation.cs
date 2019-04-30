ccf87ae2b3d4bc1d4c67d70faf9c1f57700c364a
Drew Graham <dmg9626@gmail.com>
2019-04-30 18:00:40 -0400

    added some half-implemented scripts for transformation/interaction system - will hopefully replace current InteractableObject code (will also likely borrow some code from there)

---
+++ HauntTimeUnity/Assets/Scripts/Interactable/Transformation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Transformation : MonoBehaviour
+{
+    public int priority;
+    public bool finished;
+
+    public UnityAction onFinished;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        // Set default OnFinished events
+        onFinished += (() => finished = true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Performs simple action on interactable objects.
+    /// </summary>
+    
+    // Override this method in base class, and have it call this base method at end of function
+    public virtual void Apply()
+    {
+    }
+}
---
+++ HauntTimeUnity/Assets/Scripts/Interactable/Transformations/SpriteSwap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwap : Transformation
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+
+    public override void Apply()
+    {
+        spriteRenderer.sprite = newSprite;
+
+        // Call onFinished
+        onFinished();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
