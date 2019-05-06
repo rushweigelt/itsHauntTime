cda4381824984c05ff34215960dade441b5509e7
Drew Graham <dmg9626@gmail.com>
2019-05-06 17:38:19 -0400

    re-implemented animations with transparent sprite sheets, cleaned up Human.cs a bit

--- HauntTimeUnity/Assets/Scripts/ScareTarget/Human.cs
+++ HauntTimeUnity/Assets/Scripts/ScareTarget/Human.cs
@@ -4,62 +4,32 @@ using UnityEngine;
 
 public class Human : ScareTarget
 {
-    public string name;
-    public Animator anim;
-    int idleHash = Animator.StringToHash("human_fakeIdle");
-    int gettingUpHash = Animator.StringToHash("human_gettingUp");
+    Animator anim;
+
+    public enum AnimationState { SITTING, STANDING_UP, SPOOKED, CLEANING, WALKING }
+
+    /// <summary>
+    /// Tells Animator which animation to play
+    /// </summary>
+    public AnimationState animationState;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        // TODO: set starting animation state
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            //AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-            if (inRange == true)
-            {
-                anim.SetTrigger(gettingUpHash);
-            }
-        }
-    }
-
-    void Walk()
-    {
-
-    }
-
-    void MakeNoise()
-    {
-
-    }
-
-    void setName()
-    {
-        
-    }
-
-    void getName()
-    {
-
-    }
-
-    protected new void InitialState()
-    {
-
-    }
-
-    protected new void ScaredState()
-    {
-
-    }
-
-    protected new void AlertedState()
-    {
-
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    //AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        //    if (inRange == true)
+        //    {
+        //        anim.SetTrigger(gettingUpHash);
+        //    }
+        //}
     }
 }
