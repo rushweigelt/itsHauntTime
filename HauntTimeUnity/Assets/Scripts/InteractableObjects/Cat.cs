a6b329f3784a69023986a00684e5f3f3d687802f
Rush Weigelt <rush.weigelt@drexel.edu>
2019-05-09 04:17:12 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/rushweigelt/itsHauntTime""
    This reverts commit 278cd2f71def88d1a8223a7fb5b88c3b0330672d.

---
+++ HauntTimeUnity/Assets/Scripts/InteractableObjects/Cat.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Cat : InteractableObject
+{
+
+    Animator anim;
+
+    public enum AnimationState { SLEEPING, JUMPING, HISSING }
+
+    public AnimationState animationState;
+
+    public Fan fan;
+
+    public BoxCollider2D detectRange;
+
+    /// <summary>
+    /// Event called when unplugged
+    /// </summary>
+    public UnityEvent FanRattle;
+
+    //additional box collider for hiss-range
+    public BoxCollider2D hissBox;
+
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        //Listener for rattle
+        FanRattle.AddListener(() => Jump());
+
+        hissBox.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        
+    }
+
+    public void Jump()
+    {
+        hissBox.enabled = false;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Hiss");
+            animationState = AnimationState.HISSING;
+        }
+    }
+}
