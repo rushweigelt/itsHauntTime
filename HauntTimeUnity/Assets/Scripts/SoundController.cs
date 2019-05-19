4724309f4565f3fddbd0aed399ed505df430d202
Drew Graham <dmg9626@gmail.com>
2019-05-19 16:05:58 -0400

    created SoundController (not being used yet)

---
+++ HauntTimeUnity/Assets/Scripts/SoundController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundController : Singleton<SoundController>
+{
+    public enum SoundType
+    {
+        MAIN_MENU,
+        GAME,
+
+        ROOMBA_BEEP,
+        CLANG,
+        FAN_RATTLE
+    }
+
+    [System.Serializable]
+    public class SoundTrack
+    {
+        public SoundType type;
+        public AudioClip clip;
+
+        [Range(0,1)]
+        public float volume = 1;
+    }
+
+    public List<SoundTrack> backgroundMusic;
+
+    public List<SoundTrack> soundEffects;
+    
+
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Sets background music to track associated with
+    /// </summary>
+    /// <param name="soundTrack"></param>
+    public void SetMusic(SoundType type)
+    {
+        SoundTrack soundTrack = backgroundMusic.Find(s => s.type == type);
+        audioSource.clip = soundTrack.clip;
+        audioSource.volume = soundTrack.volume;
+    }
+
+    /// <summary>
+    /// Plays sound effect associated with SoundType
+    /// </summary>
+    /// <param name="type"></param>
+    public void PlaySoundEffect(SoundType type)
+    {
+        SoundTrack track = soundEffects.Find(s => s.type == type);
+        audioSource.PlayOneShot(track.clip, track.volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
