972651881c1d9865093883e6b0f6744357267535
Drew Graham <dmg9626@gmail.com>
2019-05-20 23:35:00 -0400

    fixed Restart button to reload current scene rather than using (incorrect) hard scene reference

--- HauntTimeUnity/Assets/Scripts/LoadSceneButton.cs
+++ HauntTimeUnity/Assets/Scripts/LoadSceneButton.cs
@@ -8,14 +8,16 @@ public class LoadSceneButton : MonoBehaviour
 {
     public enum Scene {
         MAIN_MENU,
-        GAME
+        GAME,
+        CURRENT
     }
 
     public Scene scene;
 
     public Dictionary<Scene, string> sceneDict = new Dictionary<Scene, string>() {
         {Scene.MAIN_MENU, "MainMenu"},
-        {Scene.GAME, "Prototype"}
+        {Scene.GAME, "Prototype Test Floor"},
+        {Scene.CURRENT, null}
     };
 
     // Start is called before the first frame update
@@ -26,7 +28,16 @@ public class LoadSceneButton : MonoBehaviour
 
     void LoadScene()
     {
-        Debug.Log("Loading scene " + scene);
-        SceneManager.LoadScene(sceneDict[scene]);
+        if(scene.Equals(Scene.CURRENT))
+        {
+            Debug.Log("Reloading current scene");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            Debug.Log("Loading scene " + scene);
+            SceneManager.LoadScene(sceneDict[scene]);
+        }
+        
     }
 }
