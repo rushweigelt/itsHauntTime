f03b7a97308afd0b10908b7075ba35f120be9931
Tyler Sheehan <16tsheehan@gmail.com>
2019-05-22 14:38:57 -0400

    Main Menu

---
+++ HauntTimeUnity/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject currentMenu;
+
+    public void Start()
+    {
+        currentMenu = mainMenu;
+    }
+
+    public void LoadScene(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void GoNextMenu(GameObject nextMenu)
+    {
+        nextMenu.SetActive(true);
+        currentMenu.SetActive(false);
+        currentMenu = nextMenu;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
