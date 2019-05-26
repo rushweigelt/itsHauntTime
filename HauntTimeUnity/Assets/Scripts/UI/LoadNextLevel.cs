7e4e9783816017c1e5c1c87506e80f389920a67a
Lucoark <lucoark@gmail.com>
2019-05-26 08:38:28 -0400

    Basic Tutorial Structure

---
+++ HauntTimeUnity/Assets/Scripts/UI/LoadNextLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+
+
+    public void LoadScene(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ HauntTimeUnity/Assets/Scripts/UI/SwitchWallWriting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWallWriting : MonoBehaviour
+{
+
+    public GameObject[] textDisplays;
+    public int currentlyShown;
+
+    public float textTimer;
+    public float textLimit;
+
+    public void Start()
+    {
+       
+
+        textDisplays[0].SetActive(true);
+        currentlyShown = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textTimer += Time.deltaTime;
+        if(textTimer >= textLimit)
+        {
+            textTimer = 0;
+            textDisplays[currentlyShown].SetActive(false);
+            currentlyShown++;
+            if (currentlyShown == textDisplays.Length)
+            {
+                currentlyShown = 0;
+            }
+            textDisplays[currentlyShown].SetActive(true);
+        }
+    }
+}
