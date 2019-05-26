5782e4593779167932bcebf09089e0a7019d56b2
Rush Weigelt <rush.weigelt@drexel.edu>
2019-05-26 17:23:11 -0400

    Timer Works Properly, as does TMPro

--- HauntTimeUnity/Assets/Scripts/GameManager/Timer.cs
+++ HauntTimeUnity/Assets/Scripts/GameManager/Timer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Timer : MonoBehaviour
     public float seconds;
     // public float maxSecs;
     public string time;
+
+    public TextMeshProUGUI txt;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,7 @@ public class Timer : MonoBehaviour
         seconds += Time.deltaTime;
         SecondsToMmSs(seconds);
         time = SecondsToMmSs(seconds);
+        txt.text = time;
     }
     void SetTime(float seconds)
     {
@@ -30,7 +34,7 @@ public class Timer : MonoBehaviour
     }
     public string SecondsToMmSs(float seconds)
     {
-        return string.Format("{0}:{1}", Mathf.Floor(seconds/60), Mathf.RoundToInt(seconds%60));
+        return string.Format("{0}:{1}", Mathf.Floor(seconds/59), Mathf.RoundToInt(seconds%59));
     }
     void RunTimer()
     {
