816d0f9b51f7ad80fc7e8d602f75b9c09345702c
Drew Graham <Drew Graham>
2019-06-01 15:23:45 -0400

    added FanBlown component to set player blown animation when in collider (need to make it stop playing on exit/disable)

---
+++ HauntTimeUnity/Assets/Scripts/InteractableObjects/FanBlown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Calls proper player animation when player enters fan blown zone
+/// </summary>
+public class FanBlown : MonoBehaviour
+{
+    public PlayerAnimController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Player.Instance.GetComponent<PlayerAnimController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player.SetBool(PlayerAnimController.AnimState.BLOWN_AWAY, true);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        
+    }
+}
