d630eb30d93fe7d845a0787ab44f9f7ad823b282
Rush Weigelt <rush.weigelt@drexel.edu>
2019-06-12 13:12:31 -0400

    options menu implementation
    Options menu, as of now, just controls an ugly mute toggle. Will need to make custom art for it eventually.

---
+++ HauntTimeUnity/Assets/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioSource _as;
+    public bool muteOn;
+    public GameObject optionsMenu;
+    public GameObject mainMenu;
+    //public Toggle t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        muteOn = false;
+    }
+
+    public void ToggleMute(Toggle t)
+    {
+        if (t.isOn)
+        {
+            Debug.Log("muted");
+            muteOn = true;
+            _as.volume = 0f;
+        }
+        else
+        {
+            Debug.Log("Unmuted");
+            muteOn = false;
+            _as.volume = 1f;
+        }
+    }
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void closeOptions()
+    {
+        optionsMenu.SetActive(false);
+    }
+}
